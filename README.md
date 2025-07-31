05d045fd15b60e657cda4642e82a57b326a916f4
Kevin <kalanfr2@gmail.com>
2025-07-30 21:03:00 -0600

    update

---
+++ README.md
@@ -0,0 +1,14 @@
+# Ejercicio del amigo Secretos 
+
+Este proyecto es un juego sencillo en donde se tienen que agregar nombres de personas para después elegir uno de los nombres al azar
+
+## Funciones principales
+
+### agregarAmigo
+Esta función como su nombre lo dice agrega un nombre a la lista que es un arreglo (amigos = []). Obtiene el valor de campo de texto, en seguida devalúa si es nulo o no para después agregarlo al arreglo.
+
+### listarAmigos
+Obtiene la lista que existe en el HTML, en seguida itera entre los valores del arreglo para crear la lista de todos los nombres.
+
+### sortearAmigo
+Evalúa que existan valores en el arreglo para poder obtener uno lo obtiene a trabes un número randomizado que corresponde al índice de alguno de los valores del arreglo
--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+  let nombre = document.getElementById("amigo").value;
+  if (nombre) {
+    amigos.push(nombre);
+    document.getElementById("amigo").value = "";
+    console.log(amigos);
+    listarAmigos();
+  } else {
+    alert("Por favor, ingresa un nombre válido.");
+  }
+}
+
+function listarAmigos() {
+  let lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+  amigos.forEach(function (amigo) {
+    let li = document.createElement("li");
+    li.textContent = amigo;
+    lista.appendChild(li);
+  });
+}
+
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert("No hay amigos para sortear.");
+    return;
+  }
+  let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+  let amigoSorteado = amigos[indiceAleatorio];
+  document.getElementById(
+    "resultado"
+  ).textContent = `Amigo sorteado: ${amigoSorteado}`;
+  amigos = [];
+  listarAmigos();
+}
